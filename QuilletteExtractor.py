85bc6bec641ecaeab637a523e18e11d9e8b6dfa2
Steve <slarge@sfu.ca>
2018-10-24 11:33:26 -0700

    Updated extractor routines - Oct 24th

--- QuilletteExtractor.py
+++ QuilletteExtractor.py
@@ -36,6 +36,21 @@ def GetArticleContent(soup_Article):
 
 	return Content
 
+def GetArticleContent_Revised(soup_Article): 					#Revised extractor, utilizing more sophistocated tree search
+
+	TextContent = []
+	Content = soup_Article.find("div", {"class":"entry-content"}).findAll("p")
+
+	for index in range(len(Content)):
+		TextContent.append(Content[index]).encode("UTF-8")
+
+	return TextContent
+
+def GetArticleContent_Revised_2(soup_Article): 					#Revised article extractor, simpler, but also grabs <script> field following articles
+
+	Content = soup_Article.find("div",{"class":"entry-content"}).get_text().encode("UTF-8")
+	return Content
+
 def WriteArticleText(Path,Filename,ArticleContent):
 	
 	CompleteName = os.path.join(Path,Filename)
@@ -45,6 +60,14 @@ def WriteArticleText(Path,Filename,ArticleContent):
 			file1.write(ArticleContent[index].encode("UTF-8") + "\n")
 		else:
 			file1.write("HTML Parsing Error\n")
+	file1.close()
+
+def WriteArticleText_Revised(Path,Filename,ArticleContent):
+
+	CompleteName = os.path.join(Path,Filename)
+	file1 = open(CompleteName,'w')
+	for index in range(len(AricleContent)):
+		file1.write(ArticleContent[index])
 	file1.close()
 
 def GetArticleURLs_Quillette(soup_TS):
@@ -69,6 +92,26 @@ def GetArticleURLs_Quillette(soup_TS):
 
 	return ArticleURLS,ArticleTitles
 
+def GetArticleURLs_Quillette_Revised(soup):
+
+	ArticleURLs = []
+	ArticleTitles = []
+
+	for article in soup.findAll("h2",{"class":"entry-title"}):
+		ArticleURLs.append(article.find("a").get("href").encode("UTF-8"))
+		Title = article.text.encode("UTF-8")
+		CollapsedTitle = TItle.replace(" ","-")
+		ArticleTitles.append(CollapsedTitle + ".txt")
+
+	return ArticleURLs,ArticleTitles
+
+def ExtractArticleSummaries_Quillette(soup):
+
+	SummaryArray = []
+	for article in soup.findAll("div",{"class":"entry-summary"}):
+		SummaryArray.append(article.get_text().encode("UTF-8"))
+
+	return SummaryArray
 
 def UpdateDictionary(WritePath,DictName,ArticleURLs):
 
@@ -98,13 +141,7 @@ def UpdateDictionary(WritePath,DictName,ArticleURLs):
 
 		for indexInner in range(len(PreviousURLs)):
 
-			#print PreviousURLs[indexInner]
-			#print ArticleURLs[indexOuter]
-			#print str(PreviousURLs[indexInner]==ArticleURLs[indexOuter])
-
 			if(PreviousURLs[indexInner] == ArticleURLs[indexOuter]):
-				#print PreviousURLs[indexInner] + "\n"
-				#print ArticleURLs[indexOuter] + "\n"
 				RepCheck.append(1)
 				Cond = True
 
@@ -119,6 +156,15 @@ def UpdateDictionary(WritePath,DictName,ArticleURLs):
 
 	return RepCheck
 
+def WriteHTMLCode(WritePath,WriteName,Soup):
+
+	CompleteName = os.path.join(WritePath,WriteName)
+	file1 = open(CompleteName,'w')
+	file1.write(str(Soup))
+	file1.close()
+
+
+WritePath_html = "htmlCode/Quillette/"
 
 MainPage = "https://quillette.com"
 TopStoriesPage = MainPage + "/category/top-stories/"
@@ -143,15 +189,19 @@ htmlCode_SL = urlopen(SpotlightPage)
 
 #soup_Main = BeautifulSoup(htmlCode_Main,'html.parser')
 soup_TS = BeautifulSoup(htmlCode_TS,'html.parser')
+#WriteHTMLCode('htmlCode/Quillette','TS_Page.dat',soup_TS)
 
 print("Top story page extracted...\n\t...")
 
 soup_SL = BeautifulSoup(htmlCode_SL,'html.parser')
+#WriteHTMLCode('htmlCode/Quillette','SL_Page.dat',soup_SL)
 
 print("Spotlight articles extracted...\n\t...")
 
-ArticleURLs_TS,ArticleTitles_TS = GetArticleURLs_Quillette(soup_TS)
-ArticleURLs_SL,ArticleTitles_SL = GetArticleURLs_Quillette(soup_SL)
+#ArticleURLs_TS,ArticleTitles_TS = GetArticleURLs_Quillette(soup_TS)
+#ArticleURLs_SL,ArticleTitles_SL = GetArticleURLs_Quillette(soup_SL)
+ArticleURLs_TS,ArticleTitles_TS = GetArticleURLs_Quillette_Revised(soup_TS)
+ArticleURLs_SL,ArticleTitles_SL = GetArticleURLs_Quillette_Revised(soup_SL)
 
 print("Article URLS Extracted\n\n")
 
@@ -161,12 +211,18 @@ print("\t\tFound --> " + str(len(RepCheck) - sum(RepCheck)) + " <-- new articles
 for index in range(len(ArticleURLs_TS)):
 	
 	if(RepCheck[index]==0):
+
+		WriteName = "Article_TS_" + str(index) + ".dat"
+
 		htmlCode_Temp = urlopen(ArticleURLs_TS[index].encode("UTF-8"))
 		soup_Temp = BeautifulSoup(htmlCode_Temp,'html.parser')
-		ArticleContent = GetArticleContent(soup_Temp)
+		#WriteHTMLCode(WritePath_html,WriteName,soup_Temp)
+		#ArticleContent = GetArticleContent(soup_Temp)
+		ArticleContent = GetArticleContent_Revised(soup_Temp)
 
 		WriteName = ArticleTitles_TS[index]
-		WriteArticleText(WritePath,WriteName,ArticleContent)
+		#WriteArticleText(WritePath,WriteName,ArticleContent)
+		WriteArticleText_Revised(WritePath,WriteName,ArticleContent)
 
 RepCheck = UpdateDictionary(WritePath,DictName,ArticleTitles_SL)
 print("\t\tFound --> " + str(len(RepCheck) - sum(RepCheck)) + " <-- new articles at " + SpotlightPage + "\n\n")
@@ -174,12 +230,18 @@ print("\t\tFound --> " + str(len(RepCheck) - sum(RepCheck)) + " <-- new articles
 for index in range(len(ArticleURLs_SL)):
 
 	if(RepCheck[index]==0):
+
+		WriteName = "Article_SL_" + str(index) + ".dat"
+
 		htmlCode_Temp = urlopen(ArticleURLs_SL[index].encode("UTF-8"))
 		soup_Temp = BeautifulSoup(htmlCode_Temp,'html.parser')
-		ArticleContent = GetArticleContent(soup_Temp)
+		#WriteHTMLCode(WritePath_html,WriteName,soup_Temp)
+		#ArticleContent = GetArticleContent(soup_Temp)
+		ArticleContent = GetArticleURLs_Quillette_Revised(soup_Temp)
 
 		WriteName = ArticleTitles_SL[index]
-		WriteArticleText(WritePath,WriteName,ArticleContent)
+		#WriteArticleText(WritePath,WriteName,ArticleContent)
+		WriteArticleText_Revised(WritePath,WriteName,ArticleContent)
 
 print("\n\n\t\t----- Quillette Extraction Finished -----\n\n\n")
 
